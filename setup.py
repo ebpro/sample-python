bad3a8db85a118d42a7d497fa31840bb14035230
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-03-29 15:18:15 +0200

    initial release.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='sample-python',
+  author='Emmanuel BRUNO',
+  version='0.0.1',
+  description='Prints a friendly message.',
+  url='https://github.com/ebpro/sample-python',
+  scripts=['hello/helloworld.py']
+)
